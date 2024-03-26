b371c44846c45f29bab8b3feeca53f80b86c53e5
mhorky <mhorky@redhat.com>
2024-03-26 18:42:39 +0100

    feat: Add 'system.get_inventory_entry'
    This allows components to get more information about the host.

--- insights_shell/_shell/system.py
+++ insights_shell/_shell/system.py
@@ -8,13 +8,26 @@ logger = logging.getLogger(__name__)
 
 
 def is_registered() -> bool:
+    """Detect registration status."""
     logger.debug("Determining system registration status.")
+    return get_inventory_entry() is not None
+
+
+def get_inventory_entry() -> inventory.Host | None:
+    logger.debug("Requesting the host from Inventory.")
     if not os.path.isfile("/etc/insights-client/machine-id"):
-        return False
+        return None
 
     with open("/etc/insights-client/machine-id") as f:
         machine_id = f.read()
 
     hosts: list[inventory.Host]
     hosts = inventory.Inventory(inventory.InventoryConnection()).get_hosts(machine_id)
-    return len(hosts) > 0
+
+    if not hosts:
+        return None
+    if len(hosts) > 1:
+        logger.debug(
+            "Inventory returned more than one host. Using the first one and ignoring others."
+        )
+    return hosts[0]
