594602b17526c87deefd03c6c66bed5d40d2043f
mhorky <mhorky@redhat.com>
2024-04-29 16:23:09 +0200

    feat(api.dto): Log name and content of omitted fields

--- insights_nest/api/dto.py
+++ insights_nest/api/dto.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 def from_json(cls: dataclasses.dataclass, data: dict):
     """Deserialize the object from a dictionary."""
     fields: set[str] = {field.name for field in dataclasses.fields(cls)}
-    omitted: int = len({k for k in data.keys() if k not in fields})
+    omitted: dict[str, ...] = {k: v for k, v in data.items() if k not in fields}
     missing: set[str] = {
         field.name for field in dataclasses.fields(cls) if field.name not in data.keys()
     }
@@ -17,5 +17,8 @@ def from_json(cls: dataclasses.dataclass, data: dict):
     for k in missing:
         data[k] = dataclasses.MISSING
     if omitted:
-        logger.debug(f"{omitted} fields were omitted from {cls.__name__}")
+        logger.debug(
+            f"{len(omitted)} fields were omitted from {cls.__name__}: "
+            + " ".join(f"{k}={v}" for k, v in omitted.items())
+        )
     return cls(**data)
